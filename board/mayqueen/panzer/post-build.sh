7856cae95e08ebf27de97ba9ba60c8c63158755d
Grace To <sales@mayqueentech.com>
2018-10-28 19:38:43 +0800

    panzer: add create_ap relative files of configuration.

--- board/mayqueen/panzer/post-build.sh
+++ board/mayqueen/panzer/post-build.sh
@@ -5,3 +5,9 @@
 BOARD_DIR="$(dirname $0)"
 
 install -m 0644 -D ${BOARD_DIR}/rootfs_overlay/etc/inittab ${TARGET_DIR}/etc/inittab
+
+install -m755 -D ${BOARD_DIR}/rootfs_overlay/usr/bin/create_ap ${TARGET_DIR}/usr/bin/create_ap
+install -m644 -D ${BOARD_DIR}/rootfs_overlay/etc/create_ap.conf ${TARGET_DIR}/etc/create_ap.conf
+install -m644 -D ${BOARD_DIR}/rootfs_overlay/share/bash-completion/completions/create_ap ${TARGET_DIR}/share/bash-completion/completions/create_ap
+install -m644 -D ${BOARD_DIR}/rootfs_overlay/share/doc/create_ap/README.md ${TARGET_DIR}/share/doc/create_ap/README.md
+
